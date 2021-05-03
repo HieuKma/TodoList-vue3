7b56233139b2ad66b91b39800814569eeb10040c
hieuitkma <hieuitkma@gmail.com>
2021-05-03 22:49:45 +0700

    first commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  <TodoItem 
+    v-for="(todo, index) in todos" 
+    :key="index"
+    :todoProps="todo"
+  />
+  
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+import TodoItem from './TodoItem'
+
+export default {
+  name: 'Todos',
+  components: { TodoItem },
+  setup() {
+    const todos = ref(['Viec 1', 'Viec 2', 'Viec 3'])
+
+    return {
+      todos,
+    }
+  }
+}
+</script>
+
+<style></style>
